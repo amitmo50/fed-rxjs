54cf9389619fb64d94bf1881a0e1bee76b45f2ef
Amit <amit.mor@is.com>
2023-08-06 22:48:38 +0300

    chore: fix unit test

--- src/app/solution/user-list/user-list.component.spec.ts
+++ src/app/solution/user-list/user-list.component.spec.ts
@@ -1,11 +1,11 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
-import {of} from 'rxjs';
+import {of, Subject} from 'rxjs';
 import {UserListComponent} from './user-list.component';
 import {UserService} from './users.service';
 import {CountryService} from './country.service';
-import {mockCountryData, mockExpectedResult, mockUsers} from "./mock-data";
-import {NO_ERRORS_SCHEMA} from "@angular/core";
+import {mockCountryData, mockExpectedResult, mockUsers} from './mock-data';
+import {NO_ERRORS_SCHEMA} from '@angular/core';
 
 describe('UserListComponent', () => {
     let component: UserListComponent;
@@ -16,9 +16,9 @@ describe('UserListComponent', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [],
-            imports: [HttpClientTestingModule,UserListComponent],
+            imports: [HttpClientTestingModule, UserListComponent],
             providers: [UserService, CountryService],
-          schemas: [NO_ERRORS_SCHEMA]
+            schemas: [NO_ERRORS_SCHEMA]
         }).compileComponents();
     });
 
@@ -36,30 +36,26 @@ describe('UserListComponent', () => {
         expect(component).toBeTruthy();
     });
 
-    it('should load users and user card data', async () => {
+    it('should call data from userService', async () => {
         userService = TestBed.inject<UserService>(UserService);
-        countryService = TestBed.inject<CountryService>(CountryService);
-        spyOn(userService['http'], 'get').and.returnValue(of(mockUsers));
-        spyOn(UserService.prototype as any, 'getUsers').and.returnValue(of(mockUsers));
-        // spyOn(countryService['http'], 'get').and.returnValue(of(mockCountryData));
+        const userSpy = spyOn(UserService.prototype as any, 'getUsers').and.returnValue(of(mockUsers));
         component.ngOnInit();
-        // const users = spyOn(component['users$'] as any, 'subscribe')
-        expect(userService.getUsers).toHaveBeenCalled();
-        // expect(countryService.getCountry).toHaveBeenCalled();
+        expect(userSpy).toHaveBeenCalled();
+    });
 
-        expect(countryService.getCountry).toHaveBeenCalledWith('United States');
-        // expect(users).toBe([mockExpectedResult]);
+    it('should call data from countryService', async () => {
+        countryService = TestBed.inject<CountryService>(CountryService);
+        const countrySpy = spyOn(CountryService.prototype as any, 'getCountry').and.returnValue(of(mockCountryData));
+        component.ngOnInit();
+        expect(countrySpy).toHaveBeenCalled();
     });
 
     it('should unsubscribe onDestroy', () => {
-
-        const ngOnDestroySpy = spyOn(component['onDestroy$'], 'coplete').and.callThrough();
-
-        // Replace 'users$' and any other subscriptions with the mock subscriptionSpy
-        component.users$ = of(mockExpectedResult); // You can replace this with a mock observable
-
+        const onDestroySpyNext = spyOn(Subject.prototype, 'next').and.callThrough();
+        const onDestroySpyComplete = spyOn(Subject.prototype, 'complete').and.callThrough();
+        component.ngOnInit();
         component.ngOnDestroy();
-
-        expect(ngOnDestroySpy).toHaveBeenCalled();
+        expect(onDestroySpyNext).toHaveBeenCalled();
+        expect(onDestroySpyComplete).toHaveBeenCalled();
     });
 });
